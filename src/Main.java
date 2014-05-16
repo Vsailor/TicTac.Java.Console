99fc48c724451306ddc2985df5d4a07e49e4411d
Vsailor <sirius9764@gmail.com>
2014-05-17 01:43:55 +0300

    Garbage from Main was deleted

--- src/Main.java
+++ src/Main.java
@@ -9,171 +9,67 @@ public class Main extends Form {
             round = new Round();
             Square square = new Square();
             round.hello();
+            IPlayer player1 = new Human();
+            IPlayer player2;
             if (round.getTypeOfGame() == '2') {
-                Computer computer = new Computer();
-                Human human= new Human();
-                round.registration(human,computer);
-                round.setMarksForPlayers(human, computer);
 
-                if (round.start == 1) {
-                    human.setMark(MARK_X);
-                    computer.setMark(MARK_O);
-
-                } else {
-
-                    human.setMark(MARK_O);
-                    computer.setMark(MARK_X);
-
-                }
-                square.showMatrix();
-                while (true) {
-
-                    if (round.start == 1) {
-                        human.go(square,round);
-                        if (square.winner(human)) {
-
-                            square.winMessage(human);
-                            break;
-                        }
-
-                        if (square.endWithoutWinner()) {
-
-                            square.endWithoutWinnerMessage();
-                            break;
-
-                        }
-                        computer.go(square, round);
-                        square.showMatrix();
-                        if (square.winner(computer)) {
-
-                            square.winMessage(computer);
-                            break;
-                        }
-
-                        if (square.endWithoutWinner()) {
-
-                            square.endWithoutWinnerMessage();
-                            break;
-
-                        }
-                    } else {
-
-                        computer.go(square,round);
-                        square.showMatrix();
-                        if (square.winner(computer)) {
-
-                            square.winMessage(computer);
-                            break;
-                        }
-
-                        if (square.endWithoutWinner()) {
-
-                            square.endWithoutWinnerMessage();
-                            break;
-
-                        }
-                        human.go(square, round);
-                        square.showMatrix();
-                        if (square.winner(human)) {
-
-                            square.winMessage(human);
-                            break;
-                        }
-
-                        if (square.endWithoutWinner()) {
-
-                            square.endWithoutWinnerMessage();
-                            break;
-
-                        }
-                    }
-
-
-                }
+                player2 = new Computer();
 
             } else {
 
-                Human player1 = new Human();
-                Human player2 = new Human();
-                round.registration(player1, player2);
-                round.setMarksForPlayers(player1, player2);
+                player2 = new Human();
+
+            }
+            round.registration(player1,player2);
+            round.setMarksForPlayers(player1, player2);
+
+            if (round.start == 1) {
+                player1.setMark(MARK_X);
+                player2.setMark(MARK_O);
+
+            } else {
+
+                player1.setMark(MARK_O);
+                player2.setMark(MARK_X);
+
+            }
+            square.showMatrix();
+            while (true) {
+
                 if (round.start == 1) {
+                    player1.go(square,round);
+                    square.showMatrix();
+                    if (square.winner(player1)) {
 
-                    player1.setMark(MARK_X);
-                    player2.setMark(MARK_O);
-
-                } else {
-
-                    player1.setMark(MARK_O);
-                    player2.setMark(MARK_X);
-
-                }
-                square.showMatrix();
-                while (true) {
-
-                    if (round.start == 1) {
-                        player1.go(square,round);
-                        square.showMatrix();
-                        if (square.winner(player1)) {
-                            square.winMessage(player1);
-                            break;
-                        }
-
-                        if (square.endWithoutWinner()) {
-
-                            square.endWithoutWinnerMessage();
-                            break;
-
-                        }
-
-                        player2.go(square, round);
-                        square.showMatrix();
-                        if (square.winner(player2)) {
-
-                            square.winMessage(player2);
-                            break;
-                        }
-
-                        if (square.endWithoutWinner()) {
-
-                            square.endWithoutWinnerMessage();
-                            break;
-
-                        }
-                    } else {
-
-                        player2.go(square,round);
-                        square.showMatrix();
-                        if (square.winner(player2)) {
-
-                            square.winMessage(player2);
-                            break;
-                        }
-
-                        if (square.endWithoutWinner()) {
-
-                            square.endWithoutWinnerMessage();
-                            break;
-
-                        }
-
-                        player1.go(square, round);
-                        square.showMatrix();
-                        if (square.winner(player1)) {
-
-                            square.winMessage(player1);
-                            break;
-                        }
-
-                        if (square.endWithoutWinner()) {
-
-                            square.endWithoutWinnerMessage();
-                            break;
-
-                        }
+                        square.winMessage(player1);
+                        break;
                     }
 
+                    if (square.endWithoutWinner()) {
+
+                        square.endWithoutWinnerMessage();
+                        break;
+
+                    }
+                    round.start = 2;
+                } else {
+                    player2.go(square, round);
+                    square.showMatrix();
+                    if (square.winner(player2)) {
+
+                        square.winMessage(player2);
+                        break;
+                    }
+
+                    if (square.endWithoutWinner()) {
+
+                        square.endWithoutWinnerMessage();
+                        break;
+
+                    }
+                    round.start = 1;
                 }
+
 
             }
         } while (round.restartGame());
